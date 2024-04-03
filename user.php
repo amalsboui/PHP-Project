574d5595a767b6ba1e1e69594c80c404a3c4b75c
eyaa <eya.hammar@insat.ucar.tn>
2024-04-03 02:53:40 +0200

    deleted the unused files

--- user.php
+++
@@ -1,9 +0,0 @@
-<?php
-function getuserbyID($id,$db){
-    $sql=" SELECT * FROM profilechanges WHERE id=?";
-    $stmt=$db->prepare($sql);
-    $stmt->execute([$id]);
-    $user=$stmt->fetch();
-    return $user;
-
-}
